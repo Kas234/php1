63f7cf55ea71aa859faa0a696422a5b504703051
Katarzyna Węglarz <k.weglarz@op.pl>
2019-11-21 21:41:54 +0100

    dodanie tekstu

---
+++ index.php
@@ -0,0 +1,23 @@
+
+<?php
+
+$imie = "Joanna".<br/>;
+echo "Witaj świecie";
+echo "$imie, witaj na stronie";
+
+$a=3;
+$b=2;
+if ($a> $b) {
+	echo "A jest większe od B";
+}
+else {
+	echo "Źle, bo B jest większe od A";
+}
+	
+	
+	$j= 10;
+	while ($j >= 0) {
+		echo $j;
+		$j-=1;
+	}
+?>
